f8f0b8fa733016e324b091f6c91fb45efdb5e4c8
ogiwara <ogiwara.dev@gmail.com>
2018-02-17 20:55:07 +0900

    init

---
+++ http_server/lib/http_server.ex
@@ -0,0 +1,14 @@
+defmodule HttpServer do
+  def init(default_options) do
+    IO.puts "initializing plug"
+    default_options
+  end
+
+  def call(conn, options) do
+    IO.puts "calling plug"
+
+    conn
+    |> Plug.Conn.send_resp(200, "Hello world!")
+  end
+
+end
